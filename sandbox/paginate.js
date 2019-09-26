82fa1825e9afa903c19059ee4003fbc0bdb6afea
Subhadeep Majumder <subhadeep.majumder@outlook.com>
2019-09-26 09:17:13 +0530

    Paginate

--- sandbox/paginate.js
+++ sandbox/paginate.js
@@ -1,7 +1,64 @@
+const async = require('async');
+const _ = require('underscore');
 const AWS = require('aws-sdk');
-AWS.config.update({
-    region: 'eu-west-1'
-});
+AWS.config.update({ region: 'eu-west-1' });
 
-const dynamodb = new AWS.DynamoDB();
+const docClient = new AWS.DynamoDB.DocumentClient();
 
+var startKey = [];
+var results = [];
+var pages = 0;
+async.doWhilst(
+    //iterate
+    (callback)=>{
+        let params = {
+            TableName: 'td_notes',
+            Limit: 2
+        };
+
+        if(!_.isEmpty(startKey)) {
+            params.ExclusiveStartKey = startKey;
+        }
+
+        docClient.scan(params, (err, data)=>{
+            if(err) {
+                console.log(err);
+                callback(err, {});
+            } else {
+                if(typeof data.LastEvaluatedKey !== 'undefined') {
+                    startKey = data.LastEvaluatedKey;
+                } else {
+                    startKey = [];
+                }
+
+                if(!_.isEmpty(data.Items)){
+                    results = _.union(results, data.Items);
+                }
+
+                pages++;
+
+                callback(null, results);
+            }
+        });
+    },
+
+    //truth test
+    (results, callback)=>{
+        if(_.isEmpty(startKey)) {
+            return callback(null, false);;
+        } else {
+            return callback(null, true);
+        }
+    },
+
+    //callback
+    (err, data) => {
+        if(err) {
+            console.log(err);
+        } else {
+            console.log(data);
+            console.log("Item Count", data.length);
+            console.log("Pages", pages);
+        }
+    }
+);
